93fc9689a5d94f2e9063eda92f014f5154c6ebb5
HANAGEINTHEWORLD <108861997+HANAGEINTHEWORLD@users.noreply.github.com>
2022-07-08 15:35:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-require('http').createServer((req, res) => res.end('hello')).listen(process.env.PORT || 3000);
+require('http').createServer((req, res) => 
+                             
+     res.end('hanagehello')).listen(process.env.PORT || 3000);
+
+aiueo

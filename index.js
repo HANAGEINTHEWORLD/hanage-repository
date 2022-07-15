fa88ca6f1a302657fb4603b979729f434b79488c
HANAGEINTHEWORLD <108861997+HANAGEINTHEWORLD@users.noreply.github.com>
2022-07-15 11:10:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 require('http').createServer((req, res) => 
                              
      res.end(`
-             <head>
-                 <meta charset="utf-8"/>
-             </head>
-             <table border="1">
-                 <thead>
-                     <tr>
-                         <th colspan="2">いろんなけ</th>
-                     </tr>
-                 </thead>
-                 <tr>
-                     <td>hanage</td>
-                     <td>はなげ</td>
-                 </tr>
-                 <tr>
-                     <td>sunege</td>
-                     <td>すねげ</td>
-                 </tr>
-                 <tr>
-                     <td>momoge</td>
-                     <td>ももげ</td>
-                 </tr>
-                 <tr>
-                     <td>udege</td>
-                     <td>うでげ</td>
-                 </tr>
-                 <tr>
-                     <td>tinge</td>
-                     <td>ちんげ</td>
-                 </tr>
-             </table>
+             <?php
+             
+             echo "Hello World!";
+             
+             ?>
              
              `)).listen(process.env.PORT || 3000);
